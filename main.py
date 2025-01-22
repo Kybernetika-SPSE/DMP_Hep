7d7daffe5696b8ca1857c260adf35a72e7ad16de
MatousHep <hepmatous@seznam.cz>
2025-01-22 12:05:46 +0100

    main edit

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from time import sleep_ms
 from machine import Pin, PWM
 
+pulse_in = Pin(2, Pin.IN)
+speaker = Pin(6, Pin.OUT)
+output = PWM(Pin(8, Pin.OUT))
+output.freq()
 count = 0
+pulse_length = 0.2
+
 def pulse_count():
     global count
     count = count + 1
 
+    speaker.value(1)
+    sleep_ms(pulse_length)
+    speaker.value(0)
 
-pulse_in = Pin(2, Pin.IN)
-speaker = PWM(Pin(6, Pin.OUT))
-pulse_in.irq(trigger=Pin.IRQ_RISING, handler=pulse_count(x))
+pulse_in.irq(trigger=Pin.IRQ_FALLING, handler=pulse_count())
 
 while True:
     
