2daa89b567ae1f5d33678d6492b7bb0ae6a312f7
MatousHep <hepmatous@seznam.cz>
2025-01-23 10:04:17 +0100

    code fix

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from time import sleep_ms
 from machine import Pin, PWM
+from rp2 import StateMachine
+
+#import threading
 
 pulse_in = Pin(2, Pin.IN)
 speaker = Pin(6, Pin.OUT)
+test = Pin("LED",Pin.OUT)
 output = PWM(Pin(8, Pin.OUT))
-output.freq()
+output.freq(10)
 count = 0
 pulse_length = 0.2
 
-def pulse_count():
+#def pulse_reset():
+    #threading.Timer(10, pulse_reset).start()
+    #count = 0
+
+def pulse_count(zbytecne_1):
+    pulse_in.irq(handler = None)
     global count
     count = count + 1
+    print("count="+str(count))
 
-    speaker.value(1)
-    sleep_ms(pulse_length)
-    speaker.value(0)
+    test.toggle()
+    sleep_ms(500)
+    test.toggle()
+    sleep_ms(500)
 
-pulse_in.irq(trigger=Pin.IRQ_FALLING, handler=pulse_count())
+    pulse_in.irq(handler = pulse_count)
 
+    #speaker.value(1)
+    #sleep_ms(pulse_length)
+    #speaker.value(0)
+    #sleep_ms(pulse_length)
+
+pulse_in.irq(trigger=Pin.IRQ_RISING, handler=pulse_count)
 while True:
-    
+    output.duty_u16(32700)
