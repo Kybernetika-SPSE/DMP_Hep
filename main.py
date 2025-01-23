dfb30e93831161ad992ab1138e5eac4cd20af53d
MatousHep <hepmatous@seznam.cz>
2025-01-23 17:32:57 +0100

    code fix

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 from time import sleep_ms
-from machine import Pin, PWM
-from rp2 import StateMachine
+from machine import Pin, PWM, Timer
 
-#import threading
-
-pulse_in = Pin(2, Pin.IN)
+pulse_in = Pin(2, Pin.IN, Pin.PULL_DOWN)
 speaker = Pin(6, Pin.OUT)
-test = Pin("LED",Pin.OUT)
 output = PWM(Pin(8, Pin.OUT))
 output.freq(10)
 count = 0
-pulse_length = 0.2
+pulse_length = 5
+timer = Timer()
 
-#def pulse_reset():
-    #threading.Timer(10, pulse_reset).start()
-    #count = 0
+def pulse_reset(useless_1):
+    global count
+    count = 0
 
-def pulse_count(zbytecne_1):
+def pulse_count(useless_2):
     pulse_in.irq(handler = None)
     global count
     count = count + 1
     print("count="+str(count))
 
-    test.toggle()
-    sleep_ms(500)
-    test.toggle()
-    sleep_ms(500)
+    speaker.value(1)
+    sleep_ms(pulse_length)
+    speaker.value(0)
+    sleep_ms(pulse_length)
 
     pulse_in.irq(handler = pulse_count)
 
-    #speaker.value(1)
-    #sleep_ms(pulse_length)
-    #speaker.value(0)
-    #sleep_ms(pulse_length)
-
+timer.init(period=2000, mode=Timer.PERIODIC, callback=pulse_reset)
 pulse_in.irq(trigger=Pin.IRQ_RISING, handler=pulse_count)
 while True:
-    output.duty_u16(32700)
+    output.duty_u16(1456)
