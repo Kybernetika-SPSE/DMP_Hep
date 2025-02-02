8b3d74c589de97441f06f3b0a1b979a03cb96a03
MatousHep <hepmatous@seznam.cz>
2025-02-02 18:13:37 +0100

    code update and PCB

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from machine import Pin, PWM, Timer
 pulse_in = Pin(2, Pin.IN, Pin.PULL_DOWN)
 speaker = Pin(6, Pin.OUT)
 output = PWM(Pin(8, Pin.OUT))
-output.freq(10)
+output.freq(1000000)
 count = 0
-pulse_length = 5
+duty = 0
+pulse_length = 2
 timer = Timer()
 
 def pulse_reset(useless_1):
@@ -17,7 +18,10 @@ def pulse_count(useless_2):
     pulse_in.irq(handler = None)
     global count
     count = count + 1
-    print("count="+str(count))
+    #bacha jmenovatel je dan vzorokovaci frekvenci
+    global duty
+    duty = (count/1700)*55538
+    #print("count="+str(count))
 
     speaker.value(1)
     sleep_ms(pulse_length)
@@ -26,7 +30,8 @@ def pulse_count(useless_2):
 
     pulse_in.irq(handler = pulse_count)
 
-timer.init(period=2000, mode=Timer.PERIODIC, callback=pulse_reset)
-pulse_in.irq(trigger=Pin.IRQ_RISING, handler=pulse_count)
+timer.init(period=500, mode=Timer.PERIODIC, callback=pulse_reset)
+pulse_in.irq(trigger=Pin.IRQ_FALLING, handler=pulse_count)
+
 while True:
-    output.duty_u16(1456)
+    output.duty_u16(duty)
